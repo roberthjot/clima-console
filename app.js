a1b3edebe585f63fd546c5a13d51f5ae36aab3dc
Roberth <roberthjot@gmail.com>
2023-05-08 00:38:06 -0400

    list

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 require('dotenv').config()
 
-const { leerInput, inquirerMenu, pausa } = require("./helpers/inquirer");
+const { leerInput, inquirerMenu, pausa, listarLugares } = require("./helpers/inquirer");
 const Busquedas = require("./models/busquedas");
+
 
 const main = async() => {
 
@@ -17,19 +18,21 @@ const main = async() => {
             case 1:
                 // Mostrar mensaje
                 const lugar = await leerInput('Ciudad: ');
-                await busquedas.ciudad( lugar )
 
                 // Buscar los lugares
-
+                const lugares = await busquedas.ciudad(lugar);
+                
                 // Seleccionar el lugar
+                const id = await listarLugares(lugares);
+                const lugarSel = lugares.find( l => l.id === id );
 
                 // Clima
  
                 // Mostrar resultados
                 console.clear('\nInformacion de la ciudad\n'.green);
-                console.log('Ciudad', )
-                console.log('Lat', )
-                console.log('Lng', )
+                console.log('Ciudad', lugarSel.nombre)
+                console.log('Lat', lugarSel.lat)
+                console.log('Lng', lugarSel.lng)
                 console.log('Temperatura', )
                 console.log('Minima', )
                 console.log('Maxima', )
--- models/busquedas.js
+++ models/busquedas.js
@@ -25,17 +25,17 @@ class Busquedas {
             });
 
             const resp = await instance.get();
-
-            console.log(resp.data) 
-
-            return [];
+            return resp.data.features.map( lugar => ({
+                id: lugar.id,
+                nombre: lugar.place_name,
+                lng: lugar.center[0],
+                lat: lugar.center[1],
+            }));
 
         } catch (error) {
             return [];
         }
 
-        return []; // Retornar los lugares
-        
     }
 
 }
